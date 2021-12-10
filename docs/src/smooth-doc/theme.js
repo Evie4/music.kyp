dd046493c8ea3bf53659538e16c007ec73812d74
N1R0 <85079850+N1R0tka@users.noreply.github.com>
2021-12-10 15:30:58 -0800

    Delete docs/src directory

--- docs/src/smooth-doc/theme.js
+++
@@ -1,9 +0,0 @@
-import { theme as baseTheme, primaryColor } from 'smooth-doc/src/theme'
-
-export const theme = {
-  ...baseTheme,
-  colors: {
-    ...baseTheme.colors,
-    ...primaryColor('indigo'),
-  },
-}
